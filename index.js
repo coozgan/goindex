e8483729a975dc1f538dff0a17d96b59dabe553c
coozgan <coozgan@gmail.com>
2020-03-09 12:05:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var authConfig = {
-    "siteName": "GoIndex", // 网站名称
-    "root_pass": "index",  // 根目录密码，优先于.password
+    "siteName": "BekeNemen", // 网站名称
+    "root_pass": "",  // 根目录密码，优先于.password
     "version" : "1.0.6", // 程序版本
     "theme" : "material", // material  classic 
     "client_id": "202264815644.apps.googleusercontent.com",
     "client_secret": "X4Z3ca8xfWDb1Voo-F9a7ZxJ",
-    "refresh_token": "", // 授权 token
-    "root": "root" // 根目录ID
+    "refresh_token": "1//0gJEJtLP9YqXYCgYIARAAGBASNwF-L9IryDdhcXalAfkmYHOXcA4HDRXKOjTfHVrggfJgCHj0GV1R9r0AFs-Ixi9kIjIsyQ4oKhA", // 授权 token
+    "root": "1hyVckmsnOGhl49pykd_5b_UtO65doQUJ" // 根目录ID
 };
 
 var gd;
@@ -18,7 +18,7 @@ var html = `
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
   <title>${authConfig.siteName}</title>
-  <script src="//cdn.jsdelivr.net/combine/gh/jquery/jquery@3.2/dist/jquery.min.js,gh/donwa/goindex@${authConfig.version}/themes/${authConfig.theme}/app.js"></script>
+  <script src="//cdn.jsdelivr.net/combine/gh/jquery/jquery@3.2/dist/jquery.min.js,gh/coozgan/goindex@${authConfig.version}/themes/${authConfig.theme}/app.js"></script>
 </head>
 <body>
 </body>
